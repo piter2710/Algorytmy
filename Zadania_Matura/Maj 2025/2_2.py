d09f3227860e228ecbd5ade7f0e0a1e9f5585a5a
NUMER_ZDAJACEGO <egzamin@poczta.pl>
2026-02-15 15:56:59 +0100

    commit

--- Zadania_Matura/Maj 2025/2_2.py
+++ Zadania_Matura/Maj 2025/2_2.py
@@ -11,8 +11,6 @@ with open("symbole.txt") as f:
         for j in range(znaki - 2):
             if (linia1[j] == linia1[j + 1] == linia1[j + 2] == linia2[j] == linia2[j + 1] == linia2[j + 2] == linia3[j] == linia3[j + 1] == linia3[j + 2]):
                 print(f"Srodek: Numer wiersza {i + 1 + 1}, Numer pozycji w wierszu {j + 1 + 1}")
-                #Dodatkowe +1 bo plik liczy wiersze i kolumny od 1 a nie od zera jak listy
-                #print(f"{i+2} {j+2}")
                 kwadraty += 1
     print(f"Liczba kwadratow: {kwadraty}")
     f.close()
